f5c8067dcb3e150863733b837e09cfa92efa7eb6
Mahmoud Sultan <mahmoud.m.sultan36@gmail.com>
2017-11-17 19:03:43 +0200

    Email validation and front end refactors

---
+++ README.md
@@ -0,0 +1,6 @@
+Registeration System Assignment - Database Course 2017
+--------------------------------
+
+Configurations:
+- change database username and password in db_setup.php
+- run mysql instructions in tables.sql
--- assets/js/signup.js
+++ assets/js/signup.js
@@ -58,20 +58,43 @@ function displayAlert(available) {
     }
 }
 
+
+function displayEmailAlert(correct) {
+    if (!correct) {
+        $("#email").addClass("error-input").removeClass("correct-input");
+        $("#email_help").slideDown();
+    } else {
+        $("#email").removeClass("error-input").addClass("correct-input");
+        $("#email_help").slideUp();
+    }
+}
+
+function validateEmailAddress() {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test($('#email').val())
+}
+
 $(document).ready(function() {
     // validate that username is not used before
     $("#username").change(function() {
         validateUsername($("#username").val(), displayAlert);
     });
 
+    $("#email").change(function() {
+        displayEmailAlert(validateEmailAddress());
+    });
+
     // when submit is click validate first username then submit
     $("#signup_button").click(function () {
-        validateUsername($("#username").val(), function(available) {
-            if (available) {
-                submit_signup()
-            } else {
-                displayAlert(available);
-            }
-        })
+        if (!validateEmailAddress()) {
+            displayEmailAlert(false);
+        } else {
+            validateUsername($("#username").val(), function(available) {
+                if (available) {
+                    submit_signup();
+                } else {
+                    displayAlert(available);
+                }
+            })
+        }
     });
 });
